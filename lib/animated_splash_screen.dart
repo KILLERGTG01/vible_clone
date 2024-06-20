2572cd286bee0f3072b28e9f48d56961a2724813
Anurag Goel <gtg07817@gmail.com>
2024-06-21 01:46:36 +0530

    Splash Screen

---
+++ lib/animated_splash_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+//import 'package:vible_clone/home_screen.dart';
+
+class AnimatedSplashScreen extends StatefulWidget {
+  const AnimatedSplashScreen({super.key});
+  @override
+  State<AnimatedSplashScreen> createState(){
+    return _AnimatedSplashScreenState();
+  }
+}
+
+class _AnimatedSplashScreenState extends State<AnimatedSplashScreen> with TickerProviderStateMixin {
+  late AnimationController _imageController;
+  late AnimationController _vController;
+  late Animation<double> _imageAnimation;
+  late Animation<double> _vAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _imageController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _imageAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _imageController, curve: Curves.easeIn),
+    );
+
+    _vController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _vAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _vController, curve: Curves.elasticOut),
+    );
+
+    _imageController.forward().then((_) {
+      _vController.forward();
+    });
+
+    _loadResources();
+  }
+
+  Future<void> _loadResources() async {
+    // Simulate a network request or some async task
+    await Future.delayed(const Duration(seconds: 5));
+    // Navigate to the home screen
+    /*Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const HomeScreen()),
+    );*/
+  }
+
+  @override
+  void dispose() {
+    _imageController.dispose();
+    _vController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            FadeTransition(
+              opacity: _imageAnimation,
+              child: Image.asset('assets/Alexander_the_Great_standing_triumphantly_on_a_bat.png'), // Your image path
+            ),
+            ScaleTransition(
+              scale: _vAnimation,
+              child:const Text(
+                'V',
+                style: TextStyle(
+                  fontSize: 100,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
